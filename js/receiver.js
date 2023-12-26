8d1958787e9f99933e71bfda0feb1163fe8ddcef
Phi Nguyen <phinguyen@maplelabs.co>
2023-12-26 13:47:50 +0700

    - Minor change

--- js/receiver.js
+++ js/receiver.js
@@ -1,9 +1,7 @@
-
-
 main();
 
 function main() {
-	const CHANNEL 			= 'urn:x-cast:com.tvcast.screenmirror';
+	const CHANNEL 			= 'urn:x-cast:com.tvcast.chromecast';
 	const ctx 				= cast.framework.CastReceiverContext.getInstance();
 	const playerManager 	= ctx.getPlayerManager();
 	const commands      	= cast.framework.messages.Command;
@@ -19,7 +17,9 @@ function main() {
         error.reason = cast.framework.messages.ErrorReason.INVALID_PARAM;
         return error;
 	  }
-	  hideIframe();
+	  hideImageStream();
+	  imageControl.stopStream();
+      imageControl.clearImg();
       return loadRequestData;
 	});
 
@@ -29,11 +29,13 @@ function main() {
 	ctx.addCustomMessageListener(CHANNEL, function(customEvent) {
 		var js = customEvent.data;
 		if (js.type == 'iframe') {
-			showIframe();
+			showImageStream();
 			playerManager.stop();
-			document.getElementById('browserIframe').src = js.url;
+			imageControl.startStream(js.url);
+
 		}else if (js.type == 'close_browser') {
-			hideIframe();
+			hideImageStream();
+			imageControl.stopStream();
 		}
 	});
 	/*
@@ -45,13 +47,78 @@ function main() {
 	ctx.start(options);
 }
 
-function showIframe() {
+let imageControl = (function() {
+    let self = {};
+    self.$image = document.getElementById('image');
+    self.$imageWrapper = document.getElementById('image-wrapper');
+
+    self.setImgSrc = function(src) {
+        console.log('setImgSrc = ' + src);
+        self.$image.src = src;
+        self.$imageWrapper.style.display = 'block';
+    };
+
+    self.clearImg = function () {
+        self.$imageWrapper.style.display = 'none';
+    };
+
+    self.streamUrl = null;
+    self.streamErrors = 0;
+    self.streamTimeout = null;
+
+    self.startStream = function(url) {
+        self.streamErrors = 0;
+        self.streamUrl = url.replace('/1.html','');
+        self.$image.onload = function() {
+            self.streamNextFrame();
+            self.streamErrors = 0;
+        };
+        self.$image.onerror = function() {
+            self.streamNextFrame();
+            if (++self.streamErrors >= 100) {
+                console.error('Stop streaming after 100 errors');
+                self.stopStream();
+            }
+        };
+        self.streamNextFrame();
+    };
+
+    self.stopStream = function() {
+        self.streamUrl = null;
+        self.$image.onload = function() {};
+        self.$image.onerror = function() {};
+        self.clearImg();
+        hideImageStream();
+    };
+
+    self.streamNextFrame = function() {
+        if (self.streamUrl === null) {
+            self.stopStream();
+            return;
+        }
+
+        let currentTime = (new Date().getTime()) / 1000;
+        let url = self.streamUrl + '/screenmirror/name=' + currentTime
+        self.setImgSrc(url);
+
+        if (self.streamTimeout !== null) {
+            clearTimeout(self.streamTimeout);
+        }
+        self.streamTimeout = setTimeout(function() {
+            ++self.streamErrors;
+            self.streamNextFrame();
+        }, 1500);
+    };
+
+    return self;
+})();
+
+function showImageStream() {
 	document.getElementById("cast_player").style.visibility = 'hidden';
-	document.getElementById("browserIframe").style.visibility = 'visible';
+    document.getElementById("image-wrapper").style.visibility = 'visible';
 }
 
-function hideIframe() {
-	document.getElementById('browserIframe').src = '';
+function hideImageStream() {
 	document.getElementById("cast_player").style.visibility = 'visible';
-	document.getElementById("browserIframe").style.visibility = 'hidden';
+    document.getElementById("image-wrapper").style.visibility = 'hidden';
 }
